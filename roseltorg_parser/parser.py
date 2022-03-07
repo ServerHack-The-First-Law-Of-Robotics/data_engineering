0bcca59c44dbf1051286f53b9adecb14bd9f6ef7
digitaljay <pushkareva_lisa@mail.ru>
2022-03-08 01:39:46 +0500

    Fix problem with INN search (may be not in header, but in lot's description)

--- roseltorg_parser/parser.py
+++ roseltorg_parser/parser.py
@@ -7,7 +7,7 @@ class TenderParser():
     def __init__(self):
         pass
 
-    def get_tenders(self, okpds: List[str], page_num = 0):
+    def get_tenders(self, okpds: List[str], page_num=0):
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
         url = 'https://www.roseltorg.ru/procedures/search?sale=1&status%5B%5D=0&status%5B%5D=1&status%5B%5D=2&status%5B%5D=3&status%5B%5D=4&'
@@ -26,9 +26,9 @@ class TenderParser():
         all_hrefs = []
         dumpfile_name = dumpfile_name.replace('/', '')
         page_num = 0
-        while page_num<50:
+        while page_num < 50:
             try:
-                hrefs = self.get_tenders(okpds, page_num = page_num)
+                hrefs = self.get_tenders(okpds, page_num=page_num)
                 all_hrefs += hrefs
                 with open(dumpfile_name, 'a') as f:
                     for href in hrefs:
@@ -40,12 +40,13 @@ class TenderParser():
 
         return all_hrefs
 
-
     def get_buyers(self, okpd: str):
         tenders = self.get_tenders([okpd])
 
     def get_tender_inn(self, tender_link: str):
-        return self.get_tender_page(tender_link).find_all('p', class_='data-table__info')[1].contents[0].split('ИНН')[-1][:-1]
+        txt = self.get_tender_page(tender_link)
+        ind_inn = txt.index('(ИНН ')
+        return txt[ind_inn + 5: ind_inn + 15]
 
     def get_tender_page(self, tender_link: str):
         headers = {
@@ -53,7 +54,7 @@ class TenderParser():
         r = requests.get('https://www.roseltorg.ru' + tender_link, headers=headers)
         # print('got it')
         # print(r.request.url)
-        return BeautifulSoup(r.text, 'html.parser')
+        return r.text
 
     def get_tender_doc(self, tender_link: str):
         soup = self.get_tender_page(tender_link)
@@ -70,9 +71,4 @@ class TenderParser():
                 if 'file-rtf' in doc['class']:
                     print(doc['href'])
 
-
-
-
 # print(TenderParser().get_tender_inn('/procedure/0372100029922000109'))
-
-
--- roseltorg_parser/search_inns_material.py
+++ roseltorg_parser/search_inns_material.py
@@ -8,10 +8,13 @@ tender_parser = TenderParser()
 def parse_category_inns_material(category: str):
     with open('material_tenders/{}_tenders.txt'.format(category)) as f:
         tenders = [i.strip() for i in f.readlines()]
-
     inns = {}
-    counter = 0
+    mistakes = 0
     for tender_link in tenders:
+        counter = 0
+        if mistakes == 10:
+            print('Too many problems with category')
+            break
         try:
             inn = int(tender_parser.get_tender_inn(tender_link))
             if inn not in inns:
@@ -26,6 +29,7 @@ def parse_category_inns_material(category: str):
                 inns[inn] += 1
             except:
                 print("Couldsn't parse", tender_link)
+                mistakes += 1
         counter += 1
         if counter % 100 == 0:
             print('Done {} from {}'.format(counter, len(tenders)))
@@ -38,12 +42,31 @@ def parse_category_inns_material(category: str):
 
 material2inn = {}
 
+already_parsed = {
+    'азотную кислоту (50-70 г/л) и серную кислоту (8-10 г/л)': ['20.59.52.194', '20.59.56.130'],
+    'бутадиен-нитрильный каучук': ['20.17.10.141'],
+    'бутадиен-стирольные и изопреновые каучуки': ['20.17.10.130', '20.17.10.120'],
+    'бутадиен-стирольный каучук': ['20.17.10.130'],
+    'десяти-двадцатипроцентный раствор вещества н2sо4 (серная кислота)': ['20.59.52.194'],
+    'каркасная жесткая резиновая смесь': ['22.19.2'],
+    'кислоту соляную (нсl) концентрированную': ['20.59.52.194'],
+    'машинное масло совместно с сульфидом молибдена (формула моs2)': ['20.59.52.194', '20.14.73'],
+    'мыльная эмульсия': ['20.41.31'],
+    'острильная машина': ['28.41.22.140'],
+    'плетеные прокладки': ['28.14.20.230'],
+    'покрытие известково-солевого типа': ['20.30'],
+    'аммоний молибденовокислый':[],
+    'аммоний хлористый': [],
+    'барабанных вулканизаторах непрерывного действия': []
+    }
+
 with open('material2tenders.json') as f:
     materials = json.load(f)
 
 for material in materials:
-    print(material)
-    material2inn[material] = parse_category_inns_material(material.replace('/', ''))
+    if material not in already_parsed:
+        print(material)
+        material2inn[material] = parse_category_inns_material(material.replace('/', ''))
 
 with open('material2inns.json', 'w', encoding='utf-8') as f:
     json.dump(material2inn, f, ensure_ascii=False)
