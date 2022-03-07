743e15a42eccab56b637f393dfb1e59390d3f210
WoodieDudy <goshagks@yandex.ru>
2022-03-07 11:05:27 +0500

    blablalbla

--- egrul2.py
+++ egrul2.py
@@ -5,12 +5,13 @@ inn = 7707083893
 t = requests.post('https://egrul.nalog.ru/', data={'query': inn}).json()['t']
 t2 = requests.get(f'https://egrul.nalog.ru/search-result/{t}').json()
 t3 = t2['rows'][0]['t']
-requests.get(f'https://egrul.nalog.ru/vyp-request/{t3}')
-requests.get(f'https://egrul.nalog.ru/vyp-status/{t3}')
+# на всякий случай
+# requests.get(f'https://egrul.nalog.ru/vyp-request/{t3}')
+# requests.get(f'https://egrul.nalog.ru/vyp-status/{t3}')
 d = requests.get(f'https://egrul.nalog.ru/vyp-download/{t3}', stream=True)
 
 d.raise_for_status()
 
-with open('../123.pdf', 'wb') as handle:
+with open('12.pdf', 'wb') as handle:
     for block in d.iter_content(1024):
         handle.write(block)
